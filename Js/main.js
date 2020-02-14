bac85c5a9f7bec3c9c8d02f62c7b38f93e0e1404
Amr Elbardini <49646105+amrelbardini@users.noreply.github.com>
2020-02-14 20:05:06 +0200

    Responsive-Contact-Form
    Responsive-Contact-Form using BootStrap-JQuery-Css3-HTML5

---
+++ Js/main.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function ()
+{
+    //fadein a message on clicking the input field
+    $(".header .box .text").on("click", function ()
+    {
+        $(".header .box .hint").fadeIn(1000);
+
+    });
+    //fadeout a message on clicking the password field
+    $(".header .box .password").on("click", function () {
+        $(".header .box .hint").fadeOut(1000);
+
+    });
+
+
+
+});
